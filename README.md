8796f989e90759dbcf9d78db2119da8f87cecf2d
prad <prad@7bulls.com>
2014-09-18 10:32:02 +0200

    - argparse introduced to have optional dry_run and assignee params  - cred.py turned into sample  - well, documentation updated  - no unit tests! is that possible? ;)

--- README.md
+++ README.md
@@ -7,3 +7,6 @@ Requirements:
 - jira-python (tested on 0.13)
 
 
+Setup:
+- copy cred.py.sample to cred.py
+- add your data there
--- clone.py
+++ clone.py
@@ -1,54 +1,73 @@
 from jira.client import JIRA
-import sys,json
+import sys, json
 from cred import *
 
 
+def parse_args():
+    from argparse import ArgumentParser
+    ap = ArgumentParser(description='JIRA tools for anonymous project')
+    ap.add_argument('task_key',
+                    action='store')
+    # nargs '+' make append unnecessary if not wrong (we do not need a list of lists here...)
+    ap.add_argument('fix_version',
+                    action='store',
+                    nargs='+')
+    ap.add_argument('-a', dest='assignee',
+                    action='store',
+                    help='The assignee of created tickets, if different from that of the source ticket',
+                    default=None)
+    ap.add_argument('-d', dest='dry_run',
+                    action='store_true',
+                    default=False)
 
-if len(sys.argv) < 3:
-    print "Usage:"
-    print "\t clone.py task_key fix_version_1 [fix_version_2] [fix_version_3] ..."
-    sys.exit(0)
+    return ap.parse_args()
 
-j = JIRA(options = {'server':server},basic_auth=(user, passwd))
-parent = j.issue(sys.argv[1])
-project =  parent.fields().project.key
-parentSummary=parent.fields().summary
-parentKey=parent.key
-parentPrioName=parent.fields().priority.name
-parentAssignee=parent.fields().assignee.name
-taskType=parent.fields().customfield_10049.value
-wo=parent.fields().customfield_11311.value if parent.fields().customfield_11311 else None
-if parent.fields().issuetype.name=='Sub-task':
-    topTask=parent.fields().parent.key
-else:
-    topTask=parentKey
+if __name__ == '__main__':
 
-for el in sys.argv[2:]:
+    args = parse_args()
+    print args
 
-    print "Creating task for", el
-
-    versionPrefix='.'.join(el.split('.')[:2])
-    
-    issue_dict = {'project': {'key': project},
-     'summary': 'CLONE %s: %s'%(versionPrefix,parentSummary),
-     'description': '%s clone. Please see %s for details'%(versionPrefix, parentKey),
-     'issuetype': {'name': 'Sub-task'},
-     'customfield_10049' : {'value':taskType},
-     'priority': {'name':parentPrioName},
-     'parent' : {'key':topTask},
-     'assignee' : {'name': parentAssignee},
-     'fixVersions' : [{'name': el}],
-     
-    }
-    if wo:
-        issue_dict['customfield_11311'] = {'value':wo}
+    jira_client = JIRA(options={'server': server}, basic_auth=(user, passwd))
+    parent = jira_client.issue(args.task_key)
+    project = parent.fields().project.key
+    parentSummary = parent.fields().summary
+    parentKey = parent.key
+    parentPrioName = parent.fields().priority.name
+    asignee_name = args.assignee if args.assignee else parent.fields().assignee.name
+    taskType = parent.fields().customfield_10049.value
+    wo = parent.fields().customfield_11311.value if parent.fields().customfield_11311 else None
+    if parent.fields().issuetype.name == 'Sub-task':
+        topTask = parent.fields().parent.key
     else:
-        print "Warning! No WorkOrder value set in", sys.argv[1]
+        topTask = parentKey
 
+    for version in args.fix_version:
 
-    #print  issue_dict
-    r = j.create_issue(fields=issue_dict)
-    r.fields.labels.append('clone')
-    r.update(fields={"labels": r.fields.labels})
-    print "Created:", r.key
+        print "Creating task for", version
+
+        versionPrefix = '.'.join(version.split('.')[:2])
+
+        issue_dict = {'project': {'key': project},
+                      'summary': 'CLONE %s: %s' % (versionPrefix, parentSummary),
+                      'description': '%s clone. Please see %s for details' % (versionPrefix, parentKey),
+                      'issuetype': {'name': 'Sub-task'},
+                      'customfield_10049': {'value': taskType},
+                      'priority': {'name': parentPrioName},
+                      'parent': {'key': topTask},
+                      'assignee': {'name': asignee_name},
+                      'fixVersions': [{'name': version}],
+
+        }
+        if wo:
+            issue_dict['customfield_11311'] = {'value': wo}
+        else:
+            print "Warning! No WorkOrder value set in", args.task_key
+
+        if args.dry_run:
+            print "dry run mode, created issue would have the following fields\n", issue_dict
+        else:
+            r = jira_client.create_issue(fields=issue_dict)
+            r.fields.labels.append('clone')
+            r.update(fields={"labels": r.fields.labels})
+            print "Created:", r.key
 
